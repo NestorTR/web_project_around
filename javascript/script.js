8d15b5a23675465d6667bff92d4ea873d8534a1f
Nestor <nes1805@hotmail.com>
2024-07-31 17:57:09 -0600

    Proyecto 7 primera subida

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+let popupProfile = document.querySelector(".popup");
+let profileEditButton = document.querySelector(".profile__edit-button");
+let profileNameNode = document.querySelector(".profile__name");
+let profileAboutNode = document.querySelector(".profile__job");
+let formProfile = document.querySelector(".popup__input");
+let inputNameNode = formProfile.querySelector(".popup__text_title");
+let inputAboutNode = formProfile.querySelector(".popup__text_about");
+let closeProfilePopupButton = popupProfile.querySelector(".popup__close");
+let popupSaveButton = formProfile.querySelector(".popup__submit-btn");
+
+profileEditButton.addEventListener("click", function () {
+  popupProfile.classList.add("active");
+  inputNameNode.value = profileNameNode.textContent;
+  inputAboutNode.value = profileAboutNode.textContent;
+  popupSaveButton.classList.remove("active");
+});
+
+formProfile.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (inputNameNode.value !== "" && inputAboutNode.value !== "") {
+    profileNameNode.textContent = inputNameNode.value;
+    profileAboutNode.textContent = inputAboutNode.value;
+    popupSaveButton.classList.add("active");
+    popupProfile.classList.remove("active");
+  }
+});
+
+closeProfilePopupButton.addEventListener("click", function () {
+  popupProfile.classList.remove("active");
+  popupSaveButton.classList.remove("active");
+});
